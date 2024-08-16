287b91c2a7122b62273e5b42e2274e7fb1021c50
Pavel Borisov <pavellbor@gmail.com>
2024-08-16 11:52:59 +0300

    wip

--- frontend/src/widgets/notifications/index.tsx
+++ frontend/src/widgets/notifications/index.tsx
@@ -3,24 +3,46 @@ import { Notification } from "@/shared/ui/notification";
 import { nanoid } from "nanoid";
 import { ReactNode, useState } from "react";
 
+type Notification = {
+  id: string;
+  text: string;
+  timeoutId?: ReturnType<typeof setTimeout>;
+};
+
+const SHOWING_TIMEOUT_IN_MS = 3000;
+
 export const Notifications = ({ children }: { children: ReactNode }) => {
-  const [notifications, setNotifications] = useState<
-    {
-      id: string;
-      text: string;
-    }[]
-  >([]);
+  const [notifications, setNotifications] = useState<Notification[]>([]);
 
   const showNotification = (text: string) => {
-    const notification = { id: nanoid(), text };
-    setNotifications([...notifications, notification]);
+    const notification: Notification = { id: nanoid(), text };
+
+    notification.timeoutId = setTimeout(
+      () => closeNotification(notification.id),
+      SHOWING_TIMEOUT_IN_MS
+    );
+
+    setNotifications((prevNotifications) => [
+      ...prevNotifications,
+      notification,
+    ]);
   };
 
   const closeNotification = (id: string) => {
-    const filteredNotifications = notifications.filter(
-      (notification) => notification.id !== id
-    );
-    setNotifications(filteredNotifications);
+    setNotifications((prevNotifications) => {
+      const filteredNotifications = prevNotifications.filter(
+        (notification) => notification.id !== id
+      );
+
+      const notificationIndex = prevNotifications.findIndex(
+        (notification) => notification.id === id
+      );
+      if (notificationIndex !== -1) {
+        clearTimeout(prevNotifications[notificationIndex].timeoutId);
+      }
+
+      return filteredNotifications;
+    });
   };
 
   return (
